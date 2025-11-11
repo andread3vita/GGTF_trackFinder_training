8b4dca472fa357c36d562316ee11989a9963b89c
Andrea De Vita <adevita@fcc-ironic-02.cern.ch>
2025-11-11 11:28:25 +0100

    FIX

--- data_creation/condor_pipeline/IDEA/noBackground/src/run_sequence_global.sh
+++ data_creation/condor_pipeline/IDEA/noBackground/src/run_sequence_global.sh
@@ -40,66 +40,66 @@ mkdir -p ${FULLOUTDIR}
 if [[ "${TYPE}" == "Pythia" ]]
 then 
 
-      # cp $WORK_DIR/data_creation/utils/Pythia_generation/${CONFIG}.cmd ${CONFIG}_${SEED}.cmd
-      # echo "Random:seed=${SEED}" >> ${CONFIG}_${SEED}.cmd
+      cp $WORK_DIR/data_creation/utils/Pythia_generation/${CONFIG}.cmd ${CONFIG}_${SEED}.cmd
+      echo "Random:seed=${SEED}" >> ${CONFIG}_${SEED}.cmd
 
-      # k4run $WORK_DIR/data_creation/utils/Pythia_generation/pythia.py -n $NEV --Dumper.Filename out_hepmc/out_${SEED}.hepmc --Pythia8.PythiaInterface.pythiacard ${CONFIG}_${SEED}.cmd
+      k4run $WORK_DIR/data_creation/utils/Pythia_generation/pythia.py -n $NEV --Dumper.Filename out_hepmc/out_${SEED}.hepmc --Pythia8.PythiaInterface.pythiacard ${CONFIG}_${SEED}.cmd
       
-      # if [[ $VERSION -eq 3 ]]
-      # then
+      if [[ $VERSION -eq 3 ]]
+      then
 
-      #       if [[ "${TRAIN_OR_VAL}" == "train" ]]
-      #       then
+            if [[ "${TRAIN_OR_VAL}" == "train" ]]
+            then
 
-      #             ddsim --compactFile $K4GEO/FCCee/IDEA/compact/IDEA_o${OPTION}_v0${VERSION}/IDEA_o${OPTION}_v0${VERSION}.xml \
-      #                   --outputFile out_edm4hep/out_sim_edm4hep_${SEED}.root \
-      #                   --inputFiles out_hepmc/out_${SEED}.hepmc \
-      #                   --numberOfEvents $NEV \
-      #                   --random.seed $SEED \
-      #                   --steeringFile  $WORK_DIR/data_creation/condor_pipeline/IDEA/noBackground/utils/SteeringFile_IDEA_o1_v03.py \
-      #                   --part.minimalKineticEnergy "0.00*MeV"   
-      #       fi
+                  ddsim --compactFile $K4GEO/FCCee/IDEA/compact/IDEA_o${OPTION}_v0${VERSION}/IDEA_o${OPTION}_v0${VERSION}.xml \
+                        --outputFile out_edm4hep/out_sim_edm4hep_${SEED}.root \
+                        --inputFiles out_hepmc/out_${SEED}.hepmc \
+                        --numberOfEvents $NEV \
+                        --random.seed $SEED \
+                        --steeringFile  $WORK_DIR/data_creation/condor_pipeline/IDEA/noBackground/utils/SteeringFile_IDEA_o1_v03.py \
+                        --part.minimalKineticEnergy "0.00*MeV"   
+            fi
 
-      #       if [[ "${TRAIN_OR_VAL}" == "val" ]]
-      #       then
+            if [[ "${TRAIN_OR_VAL}" == "val" ]]
+            then
 
-      #             ddsim --compactFile $K4GEO/FCCee/IDEA/compact/IDEA_o${OPTION}_v0${VERSION}/IDEA_o${OPTION}_v0${VERSION}.xml \
-      #                   --outputFile out_edm4hep/out_sim_edm4hep_${SEED}.root \
-      #                   --inputFiles out_hepmc/out_${SEED}.hepmc \
-      #                   --numberOfEvents $NEV \
-      #                   --random.seed $SEED \
-      #                   --steeringFile $WORK_DIR/data_creation/condor_pipeline/IDEA/noBackground/utils/SteeringFile_IDEA_o1_v03.py \
-      #                   --part.userParticleHandler='' \
-      #                   --part.keepAllParticles true 
-      #       fi            
-      # fi   
+                  ddsim --compactFile $K4GEO/FCCee/IDEA/compact/IDEA_o${OPTION}_v0${VERSION}/IDEA_o${OPTION}_v0${VERSION}.xml \
+                        --outputFile out_edm4hep/out_sim_edm4hep_${SEED}.root \
+                        --inputFiles out_hepmc/out_${SEED}.hepmc \
+                        --numberOfEvents $NEV \
+                        --random.seed $SEED \
+                        --steeringFile $WORK_DIR/data_creation/condor_pipeline/IDEA/noBackground/utils/SteeringFile_IDEA_o1_v03.py \
+                        --part.userParticleHandler='' \
+                        --part.keepAllParticles true 
+            fi            
+      fi   
 
-      # if [[ $VERSION -eq 2 ]]
-      # then
-      #       if [[ "${TRAIN_OR_VAL}" == "train" ]]
-      #       then
+      if [[ $VERSION -eq 2 ]]
+      then
+            if [[ "${TRAIN_OR_VAL}" == "train" ]]
+            then
 
-      #             ddsim --compactFile $K4GEO/FCCee/IDEA/compact/IDEA_o${OPTION}_v0${VERSION}/IDEA_o${OPTION}_v0${VERSION}.xml \
-      #                   --outputFile out_edm4hep/out_sim_edm4hep_${SEED}.root \
-      #                   --inputFiles out_hepmc/out_${SEED}.hepmc \
-      #                   --numberOfEvents $NEV \
-      #                   --random.seed $SEED \
-      #                   --part.minimalKineticEnergy "0.00*MeV"
+                  ddsim --compactFile $K4GEO/FCCee/IDEA/compact/IDEA_o${OPTION}_v0${VERSION}/IDEA_o${OPTION}_v0${VERSION}.xml \
+                        --outputFile out_edm4hep/out_sim_edm4hep_${SEED}.root \
+                        --inputFiles out_hepmc/out_${SEED}.hepmc \
+                        --numberOfEvents $NEV \
+                        --random.seed $SEED \
+                        --part.minimalKineticEnergy "0.00*MeV"
                   
-      #       fi
+            fi
 
-      #       if [[ "${TRAIN_OR_VAL}" == "val" ]]
-      #       then
+            if [[ "${TRAIN_OR_VAL}" == "val" ]]
+            then
 
-      #             ddsim --compactFile $K4GEO/FCCee/IDEA/compact/IDEA_o${OPTION}_v0${VERSION}/IDEA_o${OPTION}_v0${VERSION}.xml \
-      #                   --outputFile out_edm4hep/out_sim_edm4hep_${SEED}.root \
-      #                   --inputFiles out_hepmc/out_${SEED}.hepmc \
-      #                   --numberOfEvents $NEV \
-      #                   --random.seed $SEED \
-      #                   --part.keepAllParticles true \
-      #                   --part.userParticleHandler=''
-      #       fi     
-      # fi
+                  ddsim --compactFile $K4GEO/FCCee/IDEA/compact/IDEA_o${OPTION}_v0${VERSION}/IDEA_o${OPTION}_v0${VERSION}.xml \
+                        --outputFile out_edm4hep/out_sim_edm4hep_${SEED}.root \
+                        --inputFiles out_hepmc/out_${SEED}.hepmc \
+                        --numberOfEvents $NEV \
+                        --random.seed $SEED \
+                        --part.keepAllParticles true \
+                        --part.userParticleHandler=''
+            fi     
+      fi
       
       k4run $WORK_DIR/data_creation/condor_pipeline/IDEA/noBackground/utils/runIDEAv${VERSION}o${OPTION}_trackerDigitizer.py --inputFile out_edm4hep/out_sim_edm4hep_${SEED}.root --outputFile out_digi/output_IDEA_DIGI_${SEED}_${TRAIN_OR_VAL}.root
       echo "Digitized simulation output file path: ${TEMP_DIR}out_digi/output_IDEA_DIGI_${SEED}_${TRAIN_OR_VAL}.root"
